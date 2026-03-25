967b50bc4141afe7a1b1f2918eae5d0d084d7d60
Omar-tn <omar.omar5750@gmail.com>
2026-03-25 15:06:35 +0200

    adding test cases for action and retry logic

---
+++ src/test/actions.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from "vitest";
+import { applyAction } from "../core/action.js";
+import {NewEvent, Pipeline} from "../db/schema";
+
+describe("applyAction - filter", () => {
+
+    const pipe = {
+        sourceKey: "test",
+        target: "http://localhost:3000/dummy-target",
+
+        action: {
+            type: "filter",
+            field: "msg",
+            equals: "hello"
+        }
+    }as Pipeline;
+
+    it("should pass when condition matches", () => {
+        const event = { payload: { msg: "hello" } } as NewEvent;
+
+
+
+        expect(applyAction(event, pipe)).toBe(true);
+    });
+
+    it("should fail when condition does not match", () => {
+        const event = { payload: { msg: "no" } } as NewEvent;
+
+
+        expect(applyAction(event, pipe)).toBe(false);
+    });
+
+});
