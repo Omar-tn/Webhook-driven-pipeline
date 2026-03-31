8484e9e40d0f685d19337aac9e3ebb1741d813e3
Omar-tn <omar.omar5750@gmail.com>
2026-04-01 00:00:12 +0300

    dockerization

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:22
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+RUN npx tsc
+
+EXPOSE 3000
+
+CMD ["sh", "-c", "npx drizzle-kit push && node dist/index.js"]
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,30 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema.js";
-const client = postgres(process.env.DB_URL!);
 
-export const db = drizzle(client,{ schema });
+
+const connectionString = process.env.DB_URL!;
+
+let client;
+
+async function connectWithRetry() {
+    let attempts = 0;
+
+    while (attempts < 10) {
+        try {
+            client = postgres(connectionString);
+            await client`SELECT 1`; // test query
+            console.log("DB connected");
+            return client;
+        } catch (err) {
+            console.log("DB not ready, retrying...");
+            attempts++;
+            await new Promise(r => setTimeout(r, 2000));
+        }
+    }
+
+    throw new Error("DB connection failed");
+}
+
+client = await connectWithRetry();
+export const db = drizzle(client, { schema });
